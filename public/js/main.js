4c4c7ab2cf1248c2be5d0013e36cc6862da73799
jktham-pc <jonas.tham@gmx.ch>
2022-04-29 18:38:46 +0200

    Get token to frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,28 @@
 const authButton = document.querySelector("#auth");
+const tokenButton = document.querySelector("#token");
+
 const vibrateButton = document.querySelector("#vibrate");
 const beepButton = document.querySelector("#beep");
 const zapButton = document.querySelector("#zap");
 
+let token = "";
+
 authButton.addEventListener("click", () => {
     window.location="/auth";
+});
+
+tokenButton.addEventListener("click", () => {
+    fetch("http://127.0.0.1:3000/token")
+        .then( response => {
+            if (response.ok) {
+                response.text().then(function (text) {
+                    token = text;
+                    tokenButton.innerHTML = "Success";
+                });                
+            } else {
+                tokenButton.innerHTML = "Failed";
+            }
+        });
 });
 
 vibrateButton.addEventListener("click", () => {
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.static('public'));
 
 let client_id = JSON.parse(fs.readFileSync("credentials.json", "utf8"))["client_id"];
 let client_secret = JSON.parse(fs.readFileSync("credentials.json", "utf8"))["client_secret"];
+let token = "";
 
 app.get("/controls", function (req, res) {
     let page = fs.readFileSync("public/index.html", "utf8");
@@ -20,10 +21,18 @@ app.get("/controls", function (req, res) {
 
 app.get("/auth", function (req, res) {
 
-    res.redirect(302, `https://pavlok-mvp.herokuapp.com/oauth/authorize?client_id=${client_id}&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Ftoken&response_type=code`);
+    res.redirect(302, `https://pavlok-mvp.herokuapp.com/oauth/authorize?client_id=${client_id}&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fredirect&response_type=code`);
 });
 
 app.get("/token", function (req, res) {
+    if (token) {
+        res.status(200).send(token);
+    } else {
+        res.sendStatus(500);
+    }
+});
+
+app.get("/redirect", function (req, res) {
     let query = url.parse(req.url, true).query;
     let code = query["code"];
 
@@ -33,13 +42,14 @@ app.get("/token", function (req, res) {
             "client_secret": client_secret,
             "code": code,
             "grant_type": "authorization_code",
-            "redirect_uri": "http://127.0.0.1:3000/token"
+            "redirect_uri": "http://127.0.0.1:3000/redirect"
         };
     
         axios.post("http://pavlok-mvp.herokuapp.com/oauth/token", data)
             .then((res) => {
-                console.log(res.data);
+                //console.log(res.data);
                 console.log("Received token:", res.data["access_token"]);
+                token = res.data["access_token"];
             }).catch((err) => {
                 console.log(err);
             });
